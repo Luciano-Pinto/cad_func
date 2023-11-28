22549c1d32796ce221e86c935b77b6e6499c6c58
lucianopinto <lcosta@prodam.am.gov.br>
2023-11-28 18:39:58 -0300

    implementação do CRUD de setores

--- cad_func/urls.py
+++ cad_func/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'cad_func'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('setores/', views.setor_list, name='setores'),
+    path('setor_create/', views.setor_create, name='setor_create'),
+    path('setor_update/<int:id>/', views.setor_update, name='setor_update'),
+    path('setor_delete/<int:id>/', views.setor_delete, name='setor_delete'),
 ]
--- cad_func/views.py
+++ cad_func/views.py
@@ -1,5 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import SetorForm
+from .models import Setor
 
 
 def home(request):
     return render(request, 'cad_func/home.html')
+
+
+def setor_list(request):
+    setores = Setor.objects.all().values()
+    context = {
+        'setores': setores,
+    }
+    return render(request, 'cad_func/setores.html', context)
+
+
+def setor_create(request):
+    if request.method == 'POST':
+        form = SetorForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('cad_func:setores')
+            except Exception as e:
+                pass
+    else:
+        form = SetorForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'cad_func/setor_create.html', context)
+
+
+def setor_update(request, id):
+    setor = Setor.objects.get(pk=id)
+    form = SetorForm(
+        initial={
+            'nome': setor.nome,
+            'data_criacao': setor.data_criacao,
+        },
+    )
+
+    if request.method == 'POST':
+        form = SetorForm(request.POST, instance=setor)
+
+        if form.is_valid():
+            try:
+                form.save()
+                model = form.instance
+                return redirect('cad_func:setores')
+            except Exception as e:
+                pass
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'cad_func/setor_update.html', context)
+
+
+def setor_delete(request, id):
+    setor = Setor.objects.get(pk=id)
+
+    try:
+        setor.delete()
+    except Exception as e:
+        pass
+
+    return redirect('cad_func:setores')
