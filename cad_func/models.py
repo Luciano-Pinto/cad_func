d65cde708c850d330935f9f10e42c0552240d725
lucianopinto <lcosta@prodam.am.gov.br>
2023-11-28 12:07:18 -0300

    criação dos models Setor e Funcionario

---
+++ cad_func/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+
+class Setor(models.Model):
+    nome = models.CharField(max_length=255)
+    data_criacao = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=255)
+    cargo = models.CharField(max_length=255)
+    data_contratacao = models.DateField(default=timezone.now)
+    setor = models.ForeignKey(Setor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
